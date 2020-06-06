ae9a1d0b77a4abe1fca313d12d3f565f28fdc962
Jay Mehta <jaymmehta97@gmail.com>
2020-06-07 02:05:35 +0530

    Added outlets, ibactions and data source methods for koloda
    - Just created a blank setup for things
    - Moved swiftlint file to parent level

--- Google Primer Mock/Controllers/HomeViewController.swift
+++ Google Primer Mock/Controllers/HomeViewController.swift
@@ -10,13 +10,51 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - UI Outlets
+
+    @IBOutlet private weak var restartButton: UIButton!
+    @IBOutlet private weak var cardsKolodaView: KolodaView!
+    @IBOutlet private weak var pageControlView: UIView!
+    @IBOutlet private weak var leftArrowButton: UIButton!
+    @IBOutlet private weak var rightArrowButton: UIButton!
+
+    // MARK: - System Preference Methods
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
+    // MARK: - Lifecycle Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        cardsKolodaView.dataSource = self
+    }
+
+    // MARK: - User interaction methods
+
+    @IBAction private func restartButtonPressed(_ sender: UIButton) {
+    }
+
+    @IBAction private func leftArrowButtonPressed(_ sender: UIButton) {
+    }
+
+    @IBAction private func rightArrowPressed(_ sender: UIButton) {
+    }
+
+}
+
+extension HomeViewController: KolodaViewDataSource {
+    // MARK: - KolodaViewDataSource Methods
+
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return 0
+    }
+
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        return UIView()
     }
 
 }
