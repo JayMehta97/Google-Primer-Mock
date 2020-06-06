c0c281e83531b20c0d0ebf872562745c7a5016b0
Jay Mehta <jaymmehta97@gmail.com>
2020-06-06 19:33:01 +0530

    Initial Commit

---
+++ Google Primer MockTests/Google_Primer_MockTests.swift
@@ -0,0 +1,34 @@
+//
+//  Google_Primer_MockTests.swift
+//  Google Primer MockTests
+//
+//  Created by Jay Mehta on 06/06/20.
+//  Copyright © 2020 Jay Mehta. All rights reserved.
+//
+
+import XCTest
+@testable import Google_Primer_Mock
+
+class Google_Primer_MockTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
